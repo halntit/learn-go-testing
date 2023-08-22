f9947985c79f7b27979040c54875ce1beb101a51
Ha Le <halntit@gmail.com>
2023-08-22 17:53:00 +0700

    test handlers

--- 1-webapp/cmd/web/handlers.go
+++ 1-webapp/cmd/web/handlers.go
@@ -3,7 +3,10 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"path"
 )
+
+var pathToTemplates = "./templates"
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	_ = app.render(w, r, "home.page.gohtml", &TemplateData{})
@@ -20,7 +23,7 @@ type TemplateData struct {
 
 func (app *application) render(w http.ResponseWriter, r *http.Request, tmpl string, data *TemplateData) error {
 	// parse the template from disk
-	parsedTemplate, err := template.ParseFiles("./templates/" + tmpl)
+	parsedTemplate, err := template.ParseFiles(path.Join(pathToTemplates, tmpl))
 	if err != nil {
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return err
---
+++ 1-webapp/cmd/web/handlers_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_application_handlers(t *testing.T) {
+	var theTests = []struct {
+		name               string
+		url                string
+		expectedStatusCode int
+	}{
+		{"home", "/", http.StatusOK},
+		{"404", "/random", http.StatusNotFound},
+	}
+
+	var app application
+	routes := app.routes()
+
+	// create a test server
+	ts := httptest.NewTLSServer(routes)
+	defer ts.Close() // as soon as test finish, close server
+
+	pathToTemplates = "./../../templates"
+
+	// range through the tests
+	for _, e := range theTests {
+		resp, err := ts.Client().Get(ts.URL + e.url)
+		if err != nil {
+			t.Log(err)
+			t.Fatal(err)
+		}
+
+		if resp.StatusCode != e.expectedStatusCode {
+			t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+		}
+	}
+}
