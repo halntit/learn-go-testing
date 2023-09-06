44a00a099351b339a372d65baa33ddc2aa8e748c
Ha Le <halntit@gmail.com>
2023-09-06 22:53:06 +0700

    create api stuff

---
+++ 1-webapp/cmd/api/api-handlers.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+)
+
+// var pathToTemplates = "./templates"
+
+func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) refresh(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) allUsers(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) getUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) updateUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) insertUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) deleteUser(w http.ResponseWriter, r *http.Request) {
+
+}
