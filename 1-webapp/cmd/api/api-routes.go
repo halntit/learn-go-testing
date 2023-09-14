0c6aa7647619cf88c8badffea34394daff23f602
Ha Le <halntit@gmail.com>
2023-09-15 00:37:33 +0700

    test api routes

--- 1-webapp/cmd/api/api-routes.go
+++ 1-webapp/cmd/api/api-routes.go
@@ -12,32 +12,22 @@ func (app *application) routes() http.Handler {
 
 	// register middleware
 	mux.Use(middleware.Recoverer)
-	// mux.Use(app.enableCORS)
+	mux.Use(app.enableCORS)
 
 	// authentication routes - auth handler, refresh
 	mux.Post("/auth", app.authenticate)
 	mux.Post("/refresh-token", app.refresh)
 
-	// test handler
-	mux.Get("/test", func(w http.ResponseWriter, r *http.Request) {
-		var payload = struct {
-			Message string `json:"message"`
-		}{
-			Message: "Hello, World!",
-		}
-
-		_ = app.writeJSON(w, http.StatusOK, payload)
-	})
-
 	// protected routes
 	mux.Route("/users", func(mux chi.Router) {
-		// user auth middleware
+		// use auth middleware
+		mux.Use(app.authRequired)
 
 		mux.Get("/", app.allUsers)
 		mux.Get("/{userID}", app.getUser)
 		mux.Delete("/{userID}", app.deleteUser)
 		mux.Put("/", app.insertUser)
-		mux.Patch("/", app.updateUser)
+		mux.Patch("/{userID}", app.updateUser)
 	})
 
 	return mux
---
+++ 1-webapp/cmd/api/api-routes_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+)
+
+// go test ./...
+// test everything in current directory and subdirectories
+
+func Test_application_routes(t *testing.T) {
+	var registered = []struct {
+		route  string
+		method string
+	}{
+		{"/auth", "POST"},
+		{"/refresh-token", "POST"},
+		{"/users/", "GET"},
+		{"/users/{userID}", "GET"},
+		{"/users/{userID}", "DELETE"},
+		{"/users/{userID}", "PATCH"},
+		{"/users/", "PUT"},
+	}
+
+	mux := app.routes()
+
+	chiRoutes := mux.(chi.Routes)
+
+	for _, route := range registered {
+		// check to see if route exists
+		if !routeExists(route.route, route.method, chiRoutes) {
+			t.Errorf("route %s %s is not registered", route.method, route.route)
+		}
+	}
+}
+
+func routeExists(testRoute, testMethod string, chiRoutes chi.Routes) bool {
+	found := false
+	_ = chi.Walk(chiRoutes, func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+		if strings.EqualFold(method, testMethod) && strings.EqualFold(route, testRoute) {
+			found = true
+		}
+		return nil
+	})
+
+	return found
+}
